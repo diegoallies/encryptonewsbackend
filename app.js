ac8f7476bf3d3c8a62c9f9502875122e1ada5bf8
Encrypto27 <diegoallies27@gmail.com>
2022-04-05 16:25:06 +0200

    save code

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 app.set('port', process.env.port || 3000) 
+app.use(cors(
 
+))
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('0223f90004ca4a288a0f604bbf9605b6');
 // To query /v2/top-headlines
@@ -13,9 +16,15 @@ app.get('/', (req, res, next)=>{
   
   newsapi.v2.topHeadlines({
     q: 'apple',
+    // language: 'en',
+    // country: 'us'
   
   }).then(response => {
-    console.log(response);
+    // console.log(response);
+    res.status(200).json({
+      status: 'ok',
+      data: response
+    })
     /*
       {
         status: "ok",
