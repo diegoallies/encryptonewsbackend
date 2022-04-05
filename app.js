b4b6395b173f70e0761b04d3e793556e0cd2b92d
Encrypto27 <diegoallies27@gmail.com>
2022-04-05 11:55:20 +0200

    added api consumption

--- app.js
+++ app.js
@@ -1,55 +1,30 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.port || 3000) 
+
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('API_KEY');
+const newsapi = new NewsAPI('0223f90004ca4a288a0f604bbf9605b6');
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query /v2/everything
-// You must include at least one q, source, or domain
-newsapi.v2.everything({
-  q: 'bitcoin',
-  sources: 'bbc-news,the-verge',
-  domains: 'bbc.co.uk, techcrunch.com',
-  from: '2017-12-01',
-  to: '2017-12-12',
-  language: 'en',
-  sortBy: 'relevancy',
-  page: 2
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  category: 'technology',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      sources: [...]
-    }
-  */
-});
+
+
+app.get('/', (req, res, next)=>{
+  
+  newsapi.v2.topHeadlines({
+    q: 'apple',
+  
+  }).then(response => {
+    console.log(response);
+    /*
+      {
+        status: "ok",
+        articles: [...]
+      }
+    */
+  });
+})
+
+app.listen(app.get('port'), server =>{
+  console.info(`Server listen on port ${app.get('port')}`);
+})
